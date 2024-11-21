c0145bc83b12475f5c01979ab21271d63306ac35
MogliDerZauberKoenig <niko_faifert@web.de>
2024-11-21 20:51:17 +0100

    Chart.js + Historie

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route("/history")
+def page_history():
+    return render_template("history.html")
+
 if __name__ == "__main":
     app.run(debug=True, host="0.0.0.0", port=5000)
