1c7c997576e267ea41d2c198781fc01ee8bbd548
MogliDerZauberKoenig <niko_faifert@web.de>
2024-11-22 15:45:59 +0100

    Settings Page

--- main.py
+++ main.py
@@ -11,5 +11,9 @@ def hello_world():
 def page_history():
     return render_template("history.html")
 
+@app.route("/settings")
+def page_settings():
+    return render_template("settings.html")
+
 if __name__ == "__main":
     app.run(debug=True, host="0.0.0.0", port=5000)
