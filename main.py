9b3aa29ab657bfb308308f1e4fd3d7bf94031583
MogliDerZauberKoenig <niko_faifert@web.de>
2025-02-03 19:57:09 +0100

    Fehler von Martin behoben.

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def get_resource(path):
     return Response(content, mimetype=mimetype)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)1
+    app.run(debug=True, host="0.0.0.0", port=8000)
